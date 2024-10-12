0454cc24600c582f57469a1df6f5940604e230d8
William David Esteban Mora <81768394+srwesteban@users.noreply.github.com>
2024-10-11 21:59:09 -0500

    final

--- script.js
+++ script.js
@@ -1,51 +1,56 @@
-const urlBase= `https://api.openweathermap.org/data/2.5/weather`
-const API_KEY= 'b06a70c9dca0f7feed1dc136f4cfe298'
-const diffKelvin = 273.15
+const urlBase = `https://api.openweathermap.org/data/2.5/weather`;
+const API_KEY = 'b06a70c9dca0f7feed1dc136f4cfe298';
+const diffKelvin = 273.15;
 
 document.getElementById('searchButton').addEventListener('click', () => {
     const city = document.getElementById('cityInput').value;
-    if(city){
-        fetchWeather(city)
-    }else{
-        alert('Ingrese una ciudad válida')
+    if (city) {
+        fetchWeather(city);
+    } else {
+        alert('Ingrese una ciudad válida');
     }
-})
+});
 
-function fetchWeather(city){
+function fetchWeather(city) {
     fetch(`${urlBase}?q=${city}&appid=${API_KEY}&lang=es`)
-    .then(data => data.json())
-    .then(data => showWeatherData(data))
+        .then(data => data.json())
+        .then(data => showWeatherData(data));
 }
 
-function showWeatherData(data){
-    const divResponseData = document.getElementById('responseData')
-    divResponseData.innerHTML = ''
+function showWeatherData(data) {
+    const divResponseData = document.getElementById('responseData');
+    divResponseData.innerHTML = '';
 
-    const cityName = data.name
-    const countryName = data.sys.country
-    const temp = data.main.temp
-    const humidity = data.main.humidity
-    const description = data.weather[0].description
-    const icon = data.weather[0].icon
+    const cityName = data.name;
+    const countryName = data.sys.country;
+    const temp = data.main.temp;
+    const humidity = data.main.humidity;
+    const description = data.weather[0].description;
+    const icon = data.weather[0].icon;
 
-    const cityInfo = document.createElement('h2')
-    cityInfo.textContent = `${cityName}, ${countryName}`
+    const cityInfo = document.createElement('h2');
+    cityInfo.textContent = `${cityName}, ${countryName}`;
 
-    const tempInfo = document.createElement('p')
-    tempInfo.textContent = `La temperatura es: ${Math.floor(temp-diffKelvin)}ºC`
+    const tempInfo = document.createElement('p');
+    tempInfo.textContent = `La temperatura es: ${Math.floor(temp - diffKelvin)}ºC`;
 
-    const humidityInfo = document.createElement('p')
-    humidityInfo.textContent = `La humedad es del ${humidity}%`
+    const humidityInfo = document.createElement('p');
+    humidityInfo.textContent = `La humedad es del ${humidity}%`;
 
-    const icoInfo = document.createElement('img')
-    icoInfo.src = `https://openweathermap.org/img/wn/${icon}@2x.png`
+    const icoInfo = document.createElement('img');
+    icoInfo.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
 
-    const descriptionInfo = document.createElement('p')
-    descriptionInfo.textContent = `La descripción meteorológica es ${description}`
+    const descriptionInfo = document.createElement('p');
+    descriptionInfo.textContent = `La descripción meteorológica es ${description}`;
 
-    divResponseData.appendChild(cityInfo)
-    divResponseData.appendChild(tempInfo)
-    divResponseData.appendChild(humidityInfo)
-    divResponseData.appendChild(icoInfo)
-    divResponseData.appendChild(descriptionInfo)
+    divResponseData.appendChild(cityInfo);
+    divResponseData.appendChild(tempInfo);
+    divResponseData.appendChild(humidityInfo);
+    divResponseData.appendChild(icoInfo);
+    divResponseData.appendChild(descriptionInfo);
 }
+
+// Establecer Pasto como ciudad por defecto al cargar la página
+window.onload = () => {
+    fetchWeather('Pasto');
+};
