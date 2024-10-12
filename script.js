4b414b2747fd1aa2738691b8a3c30ec531dfa062
William David Esteban Mora <81768394+srwesteban@users.noreply.github.com>
2024-10-11 21:43:30 -0500

    final

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const urlBase= `https://api.openweathermap.org/data/2.5/weather`
-const API_KEY= 'API-KEY'
+const API_KEY= 'b06a70c9dca0f7feed1dc136f4cfe298'
 const diffKelvin = 273.15
 
 document.getElementById('searchButton').addEventListener('click', () => {
